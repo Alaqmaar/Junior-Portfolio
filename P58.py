35cb75b8345d3e8bf7bf3e05461f1203b57c7897
Alaqmaar <75083436+Alaqmaar@users.noreply.github.com>
2021-10-28 11:05:07 +0400

    Create P58.py

---
+++ P58.py
@@ -0,0 +1,16 @@
+list1 = ['Red', 'Green', 'Blue', 'Cyan', 'Magenta', 'Yellow', 'Black']
+
+# Slicing between 2 indexes
+print(list1[2:6])
+
+# Slicing with step size
+print(list1[2:6:2])
+
+# Negative indexes
+print(list1[-6:-2])
+
+# Missing first and last indexes
+print(list1[::2])
+
+# Negative steps
+print(list1[::-1])
