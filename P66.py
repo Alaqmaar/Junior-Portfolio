78740310c2ccb223752df807a9514db10dbc4c88
Alaqmaar <75083436+Alaqmaar@users.noreply.github.com>
2021-12-05 08:38:06 +0400

    Create P66.py

---
+++ P66.py
@@ -0,0 +1,7 @@
+st = ((101, "Aman", 98), (102, "Geet", 95), (103, "Rijun", 93))
+
+print("S_no\tRoll_No\tName\tMarks")
+
+for i in range(st):
+  print(i+1, "\t", st[i][0], "\t", st[i][1], "\t", st[i][0])
+  
