8e53915938a70078bf7219f94e2517fd6068f6a2
Alaqmaar <thewardstricks@outlook.com>
2022-01-03 11:30:27 +0400

    Added P70

--- P69.py
+++ P69.py
@@ -1,7 +1,7 @@
 dict1 = {1:"One", 2:"Two", 3:"Three", 4:"Four", 5:"Five", 6:"Six", 7:"Seven", 8:"Eight", 9:"Nine", 10:"Ten"}
 
-print(dict1.key())
-print(dict1.value())
+print(dict1.keys())
+print(dict1.values())
 print(dict1)
 print(len(dict1))
 print(dict1.get(7))
---
+++ P70.py
@@ -0,0 +1,32 @@
+dict1 = {'Mohan':95, 'Ram':89, 'Suhel':92, 'Sangeeta':85}
+
+# Indexing a dictionary
+print('\nIndexing a dictionary')
+print(dict1['Mohan'])
+
+# Adding an element to a dictionary
+dict1['Meena'] = 94
+print('\nAdding an element to a dictionary')
+print(dict1)
+
+# Modifying an element in a dictionary
+dict1['Mohan'] = 80
+print('\nModifying an element in a dictionary')
+print(dict1)
+
+# Check if a key is present in a dictionary
+print('\nCheck if a key is present in a dictionary')
+print('Mohan' in dict1, '\n' + str('Rohan' in dict1))
+
+# Traversing a dictionary
+print('\nTraversing a dictionary')
+for key in dict1:
+    print(str(key) + ":" + str(dict1[key]))
+
+# Length of a dictionary
+print('\nLength of a dictionary')
+print(len(dict1), 'elements')
+
+# List of keys of a dictionary
+print('\nList of keys of a dictionary')
+print(dict1.keys())
