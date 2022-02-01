78d970df70cedccc317eb68fcef2c85976e8defd
Alaqmar <amgandhi2005@outlook.com>
2022-02-01 11:38:38 +0400

    Create discriminant.py

---
+++ discriminant.py
@@ -0,0 +1,10 @@
+print('For a quadratic equation in the form of ax^2+bx+c')
+a = int(input('Enter value of a: '))
+b = int(input('Enter value of b: '))
+c = int(input('Enter value of c: '))
+
+def discriminant(a, b, c):
+  d = b**2(-4*a*c)
+  return d
+
+print(discriminant(a, b, c)
